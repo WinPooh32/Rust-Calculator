2db7c4c08c1f093ee14972e00771a572147f6b09
WinPooh32 <deathkill2d@gmail.com>
2016-07-28 16:22:21 +1000

    Added input mask, Enter handling
    it calculates expression on Enter key pressing

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate gtk;
 pub mod stack;
 pub mod calc;
 
+// use gdk::prelude::*;
 use gtk::prelude::*;
-use gtk::{Builder, Button, Window, TextView};
+use gtk::{Builder, Button, Window, TextView, TextBuffer};
 
 fn map_number_btns(text_view: &TextView, nums_vec: &Vec<Button>) {
     let mut idx = 0;
@@ -28,6 +29,18 @@ fn map_btn_insert_at_cursor(text_view: &TextView, btn: &Button, txt: &str) {
         let buf = tv.get_buffer().unwrap();
         buf.insert_at_cursor(&txt_cpy);
     });
+}
+
+fn calc_and_set_result(in_buf: &TextBuffer, out_buf: &TextBuffer){
+    let (begin, end) = in_buf.get_bounds();
+
+    match calc::calc(&(in_buf.get_text(&begin, &end, true)).unwrap()) {
+        Ok(x) => {
+            let stringed = format!("= {}", x);
+            out_buf.set_text(&stringed);
+        },
+        Err(x) => out_buf.set_text(&x)
+    }
 }
 
 fn main() {
@@ -80,6 +93,32 @@ fn main() {
         Inhibit(false)
     });
 
+    //Clear output view on input view changed
+    let tv = text_view.clone();
+    let tv_out = output_view.clone();
+
+    text_view.connect_key_press_event(move |_, key|{
+        match key.get_keyval(){
+            65293 | 65421 => { //ENTER, num lock enter button
+                let buf = tv.get_buffer().unwrap();
+                let buf_out = tv_out.get_buffer().unwrap();
+                calc_and_set_result(&buf, &buf_out);
+                Inhibit(true)
+            },
+            48...57 | 65...90 | 97...122 | 94 | 37 | 
+            46 | 42 | 47 | 65288 | 65456...65465 | 40 | 41 |
+            65455 | 65450 | 65453 | 65451 | 65454
+            => { //nums, letters, ^, %, ., *, /, space, backspace, num lock, (, )  keys
+                tv_out.get_buffer().unwrap().set_text("");
+                Inhibit(false)
+            }, 
+            _ =>{  
+                //println!("VAL {}",  key.get_keyval());
+                Inhibit(true)
+            }
+        }
+    });
+
     // Map buttons
     map_number_btns(&text_view, &nums_vec);
 
@@ -91,16 +130,8 @@ fn main() {
         let buf = tv.get_buffer().unwrap();
         let buf_out = tv_out.get_buffer().unwrap();
 
-        let (begin, end) = buf.get_bounds();
 
-        match calc::calc(&(buf.get_text(&begin, &end, true)).unwrap()) {
-            Ok(x) => {
-                let stringed = format!("= {}", x);
-                buf_out.set_text(&stringed);
-            },
-            Err(x) => buf_out.set_text(&x)
-        }
-
+        calc_and_set_result(&buf, &buf_out);
         // let stringed = format!("{}",
         //                        calc::calc(&(buf.get_text(&begin, &end, true)).unwrap()).unwrap());
 
@@ -131,6 +162,7 @@ fn main() {
 
     map_btn_insert_at_cursor(&text_view, &btn_par_left, "(");
     map_btn_insert_at_cursor(&text_view, &btn_par_right, ")");
+
 
     window.show_all();
 
